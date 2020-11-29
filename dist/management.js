bb7c8cc9723d311c90be7011f12b9e8059a47738
Anthony Chu <antchu@microsoft.com>
2020-11-29 10:22:24 -0800

    Add database endpoint

--- dist/management.js
+++ dist/management.js
@@ -42,38 +42,185 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.swaManagementFunctionJson = exports.swaManagementFunction = void 0;
 var auth_1 = require("./auth");
 var constants_1 = __importDefault(require("./constants"));
+var mongodb_1 = require("./mongodb");
 var signalr_1 = require("./signalr");
 function swaManagementFunction(context) {
     return __awaiter(this, void 0, void 0, function () {
-        var route, _a;
+        var route, user, _a;
         return __generator(this, function (_b) {
             switch (_b.label) {
                 case 0:
                     route = context.bindingData.route;
+                    user = auth_1.decodeAuthInfo(context.req);
                     context.log("Management function triggered: " + route);
                     _a = route;
                     switch (_a) {
                         case "realtime/negotiate": return [3 /*break*/, 1];
+                        case "database/operation": return [3 /*break*/, 3];
                     }
-                    return [3 /*break*/, 3];
-                case 1: return [4 /*yield*/, signalRNegotiate(context)];
+                    return [3 /*break*/, 5];
+                case 1: return [4 /*yield*/, signalRNegotiate(context, user)];
                 case 2:
                     _b.sent();
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
+                    return [2 /*return*/];
+                case 3: return [4 /*yield*/, processDatabaseOperation(context, user)];
+                case 4:
+                    _b.sent();
+                    return [2 /*return*/];
+                case 5: return [2 /*return*/];
             }
         });
     });
 }
 exports.swaManagementFunction = swaManagementFunction;
-function signalRNegotiate(context) {
+function processDatabaseOperation(context, user) {
+    var _a, _b;
+    return __awaiter(this, void 0, void 0, function () {
+        var payload, database, collection, _c;
+        return __generator(this, function (_d) {
+            switch (_d.label) {
+                case 0:
+                    if (((_a = context.req) === null || _a === void 0 ? void 0 : _a.method) !== "POST") {
+                        if (context.res) {
+                            context.res.status = 400;
+                        }
+                        return [2 /*return*/];
+                    }
+                    if (!user) {
+                        if (context.res) {
+                            context.res.status = 401;
+                        }
+                        return [2 /*return*/];
+                    }
+                    payload = (_b = context.req) === null || _b === void 0 ? void 0 : _b.body;
+                    return [4 /*yield*/, mongodb_1.MongoDb.getClient(user)];
+                case 1:
+                    database = _d.sent();
+                    collection = database === null || database === void 0 ? void 0 : database.collection(payload.collection);
+                    if (!database || !collection || !payload.collection) {
+                        setResponse(context, 400);
+                        return [2 /*return*/];
+                    }
+                    _c = payload.operation;
+                    switch (_c) {
+                        case "getDocument": return [3 /*break*/, 2];
+                        case "findDocuments": return [3 /*break*/, 4];
+                        case "insertDocument": return [3 /*break*/, 6];
+                        case "replaceDocument": return [3 /*break*/, 8];
+                        case "deleteDocument": return [3 /*break*/, 10];
+                    }
+                    return [3 /*break*/, 12];
+                case 2: return [4 /*yield*/, getDocumentOperation(collection, payload, context)];
+                case 3:
+                    _d.sent();
+                    return [2 /*return*/];
+                case 4: return [4 /*yield*/, findDocumentsOperation(collection, payload, context)];
+                case 5:
+                    _d.sent();
+                    return [2 /*return*/];
+                case 6: return [4 /*yield*/, insertDocumentOperation(collection, payload, context)];
+                case 7:
+                    _d.sent();
+                    return [2 /*return*/];
+                case 8: return [4 /*yield*/, replaceDocumentOperation(collection, payload, context)];
+                case 9:
+                    _d.sent();
+                    return [2 /*return*/];
+                case 10: return [4 /*yield*/, deleteDocumentOperation(collection, payload, context)];
+                case 11:
+                    _d.sent();
+                    return [2 /*return*/];
+                case 12: return [2 /*return*/];
+            }
+        });
+    });
+}
+function getDocumentOperation(collection, payload, context) {
+    return __awaiter(this, void 0, void 0, function () {
+        var result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, collection.getDocument(payload._id)];
+                case 1:
+                    result = _a.sent();
+                    setResponse(context, 200, { result: result });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function findDocumentsOperation(collection, payload, context) {
+    return __awaiter(this, void 0, void 0, function () {
+        var result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, collection.findDocuments(payload.query, payload.options)];
+                case 1:
+                    result = _a.sent();
+                    setResponse(context, 200, { result: result });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function insertDocumentOperation(collection, payload, context) {
+    return __awaiter(this, void 0, void 0, function () {
+        var result;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, collection.insertDocument(payload.doc)];
+                case 1:
+                    result = _a.sent();
+                    setResponse(context, 200, { result: result });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function replaceDocumentOperation(collection, payload, context) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, collection.replaceDocument(payload.doc)];
+                case 1:
+                    _a.sent();
+                    setResponse(context, 200);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function deleteDocumentOperation(collection, payload, context) {
+    return __awaiter(this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, collection.deleteDocument(payload._id)];
+                case 1:
+                    _a.sent();
+                    setResponse(context, 200);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+function setResponse(context, status, body) {
+    if (context.res) {
+        context.res.status = status;
+        context.res.headers = {
+            "Content-Type": "application/json"
+        };
+        if (body) {
+            context.res.body = body;
+        }
+    }
+}
+function signalRNegotiate(context, user) {
     var _a;
     return __awaiter(this, void 0, void 0, function () {
-        var user, client, _i, _b, role;
+        var client, _i, _b, role;
         return __generator(this, function (_c) {
             switch (_c.label) {
                 case 0:
-                    user = auth_1.decodeAuthInfo(context.req);
                     client = signalr_1.SignalRClient.fromConnectionString();
                     if (!user) return [3 /*break*/, 7];
                     return [4 /*yield*/, client.removeUserFromAllGroups(user.userId)];
--- management.ts
+++ management.ts
@@ -1,20 +1,116 @@
 import { Context } from "@azure/functions";
-import { decodeAuthInfo } from "./auth";
+import { AuthenticatedUser, decodeAuthInfo } from "./auth";
 import constants from "./constants";
+import { Collection, FindDocumentsOptions } from "./database";
+import { MongoDb } from "./mongodb";
 import { SignalRClient } from "./signalr";
 
 async function swaManagementFunction(context: Context): Promise<void> {
     const route = context.bindingData.route;
+    const user = decodeAuthInfo(context.req);
     context.log(`Management function triggered: ${route}`);
     switch (route) {
         case "realtime/negotiate":
-            await signalRNegotiate(context)
-            break;
+            await signalRNegotiate(context, user);
+            return;
+        case "database/operation":
+            await processDatabaseOperation(context, user);
+            return;
     }
 }
 
-async function signalRNegotiate(context: Context): Promise<void> {
-    const user = decodeAuthInfo(context.req);
+async function processDatabaseOperation(context: Context, user?: AuthenticatedUser): Promise<void> {
+    if (context.req?.method !== "POST") {
+        if (context.res) {
+            context.res.status = 400;
+        }
+        return;
+    }
+
+    if (!user) {
+        if (context.res) {
+            context.res.status = 401;
+        }
+        return;
+    }
+
+    // TODO: perform authorization here
+
+    const payload = context.req?.body as DatabaseOperation;
+    const database = await MongoDb.getClient(user);
+    const collection = database?.collection(payload.collection);
+
+    if (!database || !collection || !payload.collection) {
+        setResponse(context, 400);
+        return;
+    }
+
+    switch (payload.operation) {
+        case "getDocument":
+            await getDocumentOperation(collection, payload, context);
+            return;
+        case "findDocuments":
+            await findDocumentsOperation(collection, payload, context);
+            return;
+        case "insertDocument":
+            await insertDocumentOperation(collection, payload, context);
+            return;
+        case "replaceDocument":
+            await replaceDocumentOperation(collection, payload, context);
+            return;
+        case "deleteDocument":
+            await deleteDocumentOperation(collection, payload, context);
+            return;
+    }
+}
+
+async function getDocumentOperation(collection: Collection, payload: DatabaseOperation, context: Context) {
+    const result = await collection.getDocument(payload._id);
+    setResponse(context, 200, { result });
+}
+
+async function findDocumentsOperation(collection: Collection, payload: DatabaseOperation, context: Context) {
+    const result = await collection.findDocuments(payload.query, payload.options);
+    setResponse(context, 200, { result });
+}
+
+async function insertDocumentOperation(collection: Collection, payload: DatabaseOperation, context: Context) {
+    const result = await collection.insertDocument(payload.doc);
+    setResponse(context, 200, { result });
+}
+
+async function replaceDocumentOperation(collection: Collection, payload: DatabaseOperation, context: Context) {
+    await collection.replaceDocument(payload.doc);
+    setResponse(context, 200);
+}
+
+async function deleteDocumentOperation(collection: Collection, payload: DatabaseOperation, context: Context) {
+    await collection.deleteDocument(payload._id);
+    setResponse(context, 200);
+}
+
+function setResponse(context: Context, status: number, body?: unknown) {
+    if (context.res) {
+        context.res.status = status;
+        context.res.headers = {
+            "Content-Type": "application/json"
+        };
+        if (body) {
+            context.res.body = body;
+        }
+    }
+}
+
+interface DatabaseOperation {
+    operation: "insertDocument" | "replaceDocument" | "deleteDocument" | "getDocument" | "findDocuments";
+    collection: string;
+    _id: string;
+    doc: { [field: string]: unknown };
+    query?: { [field: string]: unknown };
+    options?: FindDocumentsOptions;
+}
+
+async function signalRNegotiate(context: Context, user?: AuthenticatedUser): Promise<void> {
     const client = SignalRClient.fromConnectionString();
     if (user) {
         await client.removeUserFromAllGroups(user.userId);
@@ -28,7 +124,7 @@ async function signalRNegotiate(context: Context): Promise<void> {
     context.res?.json(client.generateNegotiatePayload(user?.userId));
 }
 
-function getUserDetailGroupName (userDetails: string) : string {
+function getUserDetailGroupName(userDetails: string): string {
     return `userdetail_${userDetails.replace(/[^A-Za-z0-9]/g, "_")}`;
 }
 
